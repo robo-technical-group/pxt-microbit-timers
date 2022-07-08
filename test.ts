9934f4ead0b9cfe5e6741d1d2810adb43308af19
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2022-07-08 00:17:41 -0400

    Update images

--- test.ts
+++ test.ts
@@ -1,14 +1,13 @@
-/* Internal tests
-let tid: number = setInterval(function () { basic.showNumber(control.millis()) }, 5000)
-basic.showNumber(tid)
-basic.clearScreen()
-basic.showNumber(setTimeout(function () { basic.showNumber(control.millis()) }, 500))
-basic.clearScreen()
-basic.showNumber(setTimeout(function () { clearInterval(tid) }, 30000))
-*/
-
 function printTime(): void {
     basic.showNumber(control.millis())
 }
 timer.after(500, printTime)
 timer.debounce("X", 5000, printTime)
+
+/* Internal tests */
+let tid: number = timer.setInterval(function () { basic.showNumber(control.millis()) }, 5000)
+basic.showNumber(tid)
+basic.clearScreen()
+basic.showNumber(timer.setTimeout(function () { basic.showNumber(control.millis()) }, 500))
+basic.clearScreen()
+basic.showNumber(timer.setTimeout(function () { timer.clearInterval(tid) }, 30000))
